594b6e89d9d8b5f2918f0a229b904300bb056cb3
Rulee Chen <rulee@live.cn>
2018-11-14 12:29:37 +0800

    update to wix 311

--- install.js
+++ install.js
@@ -4,7 +4,7 @@ var path = require('path');
 var os = require('os');
 var unzip = require('unzip');
 
-var WIX_BINARY_URL = 'http://static.wixtoolset.org/releases/v3.9.1006.0/wix39-binaries.zip'
+var WIX_BINARY_URL = 'https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311-binaries.zip';
 
 var zipPath = path.resolve(os.tmpdir(), 'wix.zip');
 var file = fs.createWriteStream(zipPath);
