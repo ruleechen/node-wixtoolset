2cef53e79b269390081a9b10cb9b1dd9e08a0cc6
Rulee Chen <rulee@live.cn>
2018-11-14 14:09:48 +0800

    introduce "download" module

--- install.js
+++ install.js
@@ -1,25 +1,10 @@
-var http = require('http');
-var https = require('https');
-var fs = require('fs');
 var path = require('path');
-var os = require('os');
-var unzip = require('unzip');
+var download = require('download');
 
 var WIX_BINARY_URL = 'https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311-binaries.zip';
 
-var zipPath = path.resolve(os.tmpdir(), 'wix.zip');
-var file = fs.createWriteStream(zipPath);
-var tool = (WIX_BINARY_URL.indexOf('https://') === 0 ? https : http);
-var request = tool.get(WIX_BINARY_URL, function (response) {
-  response.pipe(file);
-  console.log('Downloading WIX Binaries');
-  response.on('data', function () {
-    process.stdout.write(".");
-  });
-  response.on('end', function () {
-    console.log('Extracting');
-    fs.createReadStream(zipPath).pipe(unzip.Extract({
-      path: path.resolve(__dirname, 'wix-bin')
-    }));
-  })
+console.log('Start downloading wix binaries...');
+var dest = path.resolve(__dirname, 'wix-bin');
+download(WIX_BINARY_URL, dest, { extract: true }).then(() => {
+  console.log('Done downloading wix binaries');
 });
