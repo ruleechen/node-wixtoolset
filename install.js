dc64657d353972f4f59ca5ea93f6ec851695962b
Rulee Chen <rulee@live.cn>
2018-11-19 15:44:51 +0800

    add wix binaries version checking

--- install.js
+++ install.js
@@ -1,10 +1,20 @@
+var fs = require('fs');
 var path = require('path');
 var download = require('download');
 
 var WIX_BINARY_URL = 'https://github.com/wixtoolset/wix3/releases/download/wix3111rtm/wix311-binaries.zip';
+var dest = path.resolve(__dirname, 'wix-bin');
+var versionPath = path.resolve(dest, 'version.txt');
+if (fs.existsSync(versionPath)) {
+  var url = fs.readFileSync(versionPath, 'utf8');
+  if (url === WIX_BINARY_URL) {
+    console.log('Wix binaries exists...');
+    return;
+  }
+}
 
 console.log('Start downloading wix binaries...');
-var dest = path.resolve(__dirname, 'wix-bin');
 download(WIX_BINARY_URL, dest, { extract: true }).then(() => {
+  fs.writeFileSync(versionPath, WIX_BINARY_URL, { encoding: 'utf8' });
   console.log('Done downloading wix binaries');
 });
